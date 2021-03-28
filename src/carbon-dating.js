62f596a5afc1de563a7a7fa7becd9761d35853ac
Stepan <lavash@tut.by>
2021-03-28 18:17:41 +0300

    add solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,29 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const ln= 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+  if(!sampleActivity) return false  
+  if(typeof sampleActivity !== "string") return false  
+  if(!isFinite(sampleActivity)) return false
+  if(Number(sampleActivity) <=0) return false
+  if(Number(sampleActivity) >15) return false
+  
+  let A = Number(sampleActivity);
+  let num = ((Math.log(MODERN_ACTIVITY/A))/(ln/HALF_LIFE_PERIOD));
+  return Math.ceil(num);
+
+ 
+  
+  
+ 
+   
+  
+  
+  
+  
+ 
+} 
+
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  
+  /*let counter = 0
+  for (let cat of matrix) {
+    if (cat="^^") {
+      counter++;
+    }
+  }
+  return counter
+}*/
+
+let count= 0, i;
+while((i= matrix.indexOf("^^", i))!= -1){
+    count++;
+    i++;
+}
+return count
+}
